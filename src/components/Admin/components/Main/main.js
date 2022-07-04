752358a26eb98f3aa59552cad848f71314f42b8c
vinusmavani <dioneapps111@gmail.com>
2022-07-04 09:48:00 +0530

    some changes

--- src/components/Admin/components/Main/main.js
+++ src/components/Admin/components/Main/main.js
@@ -7,12 +7,12 @@ export default function Main() {
     return (
         // <div className="">
         <div className='grid col-12 card m-0'>
-            <div className='grid col-5'>
+            <div className='grid col-5' >
                 <div className='col-6 mkg p-d-flex'>
                     <label htmlFor="username2" className="input-label pr-2">Username</label>
                     <InputText value={value1} className="sm-input" onChange={(e) => setValue1(e.target.value)} />
                 </div>
-                <div className='col-6 p-d-flex'>
+                <div className='col-6 mkg p-d-flex'>
                     <label htmlFor="username2" className="input-label pr-2">Username</label>
                     <InputText value={value1} className="sm-input" onChange={(e) => setValue1(e.target.value)} />
                 </div>
@@ -23,7 +23,7 @@ export default function Main() {
             <div className='col-3 '>
 
             </div>
-        </div>
+        </div >
         // </div>
     );
 }
