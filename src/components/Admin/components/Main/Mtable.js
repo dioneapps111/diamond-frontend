c3384184d5fbd523abcd4a4462b4054f7e64d859
dioneapps <dioneapps111@gmail.com>
2022-07-13 18:07:49 +0530

    add dialog table

--- src/components/Admin/components/Main/Mtable.js
+++ src/components/Admin/components/Main/Mtable.js
@@ -1,11 +1,115 @@
-// import React, { useState, useEffect } from "react";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 const DataTableGridLinesDemo = () => {
-
+  const [data, setData] = useState([
+    {
+      refno: "A1",
+      lotno: "FANCY YELLOW VS1 CUSHION",
+      pcs: "1",
+      carat: "10.2",
+      rate: "200",
+      amount: "2040",
+      l1: "1",
+      l2: "2",
+      net_amount: "1950",
+      kapan: "S1",
+      remark: "ok",
+      asking_rate: "1000",
+    },
+    {
+      refno: "A1",
+      lotno: "FANCY YELLOW VS1 CUSHION",
+      pcs: "1",
+      carat: "10.2",
+      rate: "200",
+      amount: "2040",
+      l1: "1",
+      l2: "2",
+      net_amount: "1950",
+      kapan: "S1",
+      remark: "ok",
+      asking_rate: "1000",
+    },
+  ]);
+  useEffect(() => {
+    let cnt = data.length - 1;
+    if (
+      data[cnt].refno !== "" ||
+      data[cnt].lotno !== "" ||
+      data[cnt].pcs !== "" ||
+      data[cnt].carat !== "" ||
+      data[cnt].rate !== "" ||
+      data[cnt].amount !== "" ||
+      data[cnt].l1 !== "" ||
+      data[cnt].l2 !== "" ||
+      data[cnt].net_amount !== "" ||
+      data[cnt].kapan !== "" ||
+      data[cnt].remark !== "" ||
+      data[cnt].asking_rate !== ""
+    ) {
+      let newData = [
+        ...data,
+        {
+          refno: "",
+          lotno: "",
+          pcs: "",
+          carat: "",
+          rate: "",
+          amount: "",
+          l1: "",
+          l2: "",
+          net_amount: "",
+          kapan: "",
+          remark: "",
+          asking_rate: "",
+        },
+      ];
+      setData(newData);
+    }
+  }, [data]);
+  const setDataOnChange = (feild, index, value) => {
+    let mg = data;
+    console.log();
+    if (feild === "refno") {
+      mg[index].refno = value;
+    }
+    if (feild === "lotno") {
+      mg[index].lotno = value;
+    }
+    if (feild === "pcs") {
+      mg[index].pcs = value;
+    }
+    if (feild === "carat") {
+      mg[index].carat = value;
+    }
+    if (feild === "rate") {
+      mg[index].rate = value;
+    }
+    if (feild === "amount") {
+      mg[index].amount = value;
+    }
+    if (feild === "l1") {
+      mg[index].l1 = value;
+    }
+    if (feild === "l2") {
+      mg[index].l2 = value;
+    }
+    if (feild === "net_amount") {
+      mg[index].net_amount = value;
+    }
+    if (feild === "kapan") {
+      mg[index].kapan = value;
+    }
+    if (feild === "remark") {
+      mg[index].remark = value;
+    }
+    if (feild === "asking_rate") {
+      mg[index].asking_rate = value;
+    }
+    setData([...mg]);
+  };
   return (
-    <div className="col-12 tbl m-0 p-0 py-2 p-fluid">
-      <table className="data-table">
+    <div className="col-12 tbl p-0 py-2 p-fluid">
+      <table className="data-table ">
         <thead>
           <tr>
             <th>NO.</th>
@@ -25,27 +129,146 @@ const DataTableGridLinesDemo = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>50mt</td>
-            <td>50mt</td>
-            <td>9mm</td>
-            <td>9mm</td>
-            <td>9mm</td>
-            <td>1/2"</td>
-            <td>1/2"</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-            <td>Kangal / Coil</td>
-          </tr>
+          {data.map((eliment, index) => {
+            return (
+              <tr key={"lot" + index}>
+                <td>{index + 1}</td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="refno"
+                    onChange={(e) => {
+                      setDataOnChange("refno", index, e.target.value);
+                    }}
+                    value={eliment.refno}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="lotno"
+                    onChange={(e) => {
+                      setDataOnChange("lotno", index, e.target.value);
+                    }}
+                    value={eliment.lotno}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="pcs"
+                    onChange={(e) => {
+                      setDataOnChange("pcs", index, e.target.value);
+                    }}
+                    value={eliment.pcs}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="carat"
+                    onChange={(e) => {
+                      setDataOnChange("carat", index, e.target.value);
+                    }}
+                    value={eliment.carat}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="rate"
+                    onChange={(e) => {
+                      setDataOnChange("rate", index, e.target.value);
+                    }}
+                    value={eliment.rate}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="amount"
+                    onChange={(e) => {
+                      setDataOnChange("amount", index, e.target.value);
+                    }}
+                    value={eliment.amount}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="l1"
+                    onChange={(e) => {
+                      setDataOnChange("l1", index, e.target.value);
+                    }}
+                    value={eliment.l1}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="l2"
+                    onChange={(e) => {
+                      setDataOnChange("l2", index, e.target.value);
+                    }}
+                    value={eliment.l2}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="net_amount"
+                    onChange={(e) => {
+                      setDataOnChange("net_amount", index, e.target.value);
+                    }}
+                    value={eliment.net_amount}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="kapan"
+                    onChange={(e) => {
+                      setDataOnChange("kapan", index, e.target.value);
+                    }}
+                    value={eliment.kapan}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="remark"
+                    onChange={(e) => {
+                      setDataOnChange("remark", index, e.target.value);
+                    }}
+                    value={eliment.remark}
+                  ></input>
+                </td>
+                <td >
+                  <input
+                    className="input"
+                    type="text"
+                    name="asking_rate"
+                    onChange={(e) => {
+                      setDataOnChange("asking_rate", index, e.target.value);
+                    }}
+                    value={eliment.asking_rate}
+                  ></input>
+                </td>
+                <td >DELETE</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
--- src/components/Admin/components/Main/XlsxBtn.js
+++ src/components/Admin/components/Main/XlsxBtn.js
@@ -1,81 +1,284 @@
-import React, { useState } from 'react'
-import { Button } from 'primereact/button';
-import { Dialog } from 'primereact/dialog';
+import React, { useEffect, useState } from "react";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import temp from "./temp";
+import { Dropdown } from 'primereact/dropdown';
 
 export const XlsxBtn = () => {
-    const [displayResponsive, setDisplayResponsive] = useState(false);
-    const [position, setPosition] = useState('center');
+  const [displayResponsive, setDisplayResponsive] = useState(true);
+  // const [position, setPosition] = useState('center');
+  const [selectedCity2, setSelectedCity2] = useState(null);
+  const dialogFuncMap = {
+    displayResponsive: setDisplayResponsive,
+  };
 
-    const dialogFuncMap = {
-        'displayResponsive': setDisplayResponsive
-    }
+  const onClick = (name, position) => {
+    dialogFuncMap[`${name}`](true);
 
-    const onClick = (name, position) => {
-        dialogFuncMap[`${name}`](true);
-
-        if (position) {
-            setPosition(position);
-        }
-    }
-    const onHide = (name) => {
-        dialogFuncMap[`${name}`](false);
-    }
-    const renderFooter = (name) => {
-        return (
-            <div className='grid col-12 downbtnlot'>
-                <Button className='btns mr-3' label="RESET" />
-                <Button className='btns mr-3' label="DELETE" />
-                <Button className='btns mr-3' label="SAVE" />
-            </div>
-        );
-    }
-    const renderHeader = (name) => {
-        return (
-            <div>
-                <Button className='btns mr-3' label="DEMO" onClick={() => onClick('displayResponsive')} />
-                <Button className='btns mr-3' label="IMPORT" onClick={() => onClick('displayResponsive')} />
-            </div>
-        );
-    }
+    // if (position) {
+    //     setPosition(position);
+    // }
+  };
+  const onHide = (name) => {
+    dialogFuncMap[`${name}`](false);
+  };
+  const renderFooter = (name) => {
     return (
-        <div className='grid col-12 btnparent my-1'>
-            <Button className='btns mr-3' label="ADD LOT" onClick={() => onClick('displayResponsive')} />
-            <Dialog header={renderHeader('displayResponsive')} visible={displayResponsive} onHide={() => onHide('displayResponsive')} breakpoints={{ '960px': '75vw' }} style={{ width: '73vw' }} footer={renderFooter('displayResponsive')}>
-                <div className="col-12 tbl m-0 p-0 p-fluid">
-                    <table className="data-table">
-                        <thead>
-                            <tr>
-                                <th>NO.</th>
-                                <th>REF NO</th>
-                                <th>LOT NO</th>
-                                <th>PCS</th>
-                                <th>CARAT</th>
-                                <th>RATE</th>
-                                <th>AMOUNT</th>
-                                <th>L1</th>
-                                <th>L2</th>
-                                <th>NET AMOUNT</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>50mt</td>
-                                <td>9mm</td>
-                                <td>9mm</td>
-                                <td>1/2"</td>
-                                <td>1/2"</td>
-                                <td>Kangal / Coil</td>
-                                <td>Kangal / Coil</td>
-                                <td>Kangal / Coil</td>
-                                <td>Kangal / Coil</td>
-                                <td>Kangal / Coil</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </Dialog>
-            <Button className='btns mr-3' label="DEMO" />
-            <Button className='btns mr-3' label="IMPORT" />
+      <div className="grid col-12 downbtnlot">
+        <Button className="btns mr-3" label="RESET" />
+        <Button className="btns mr-3" label="DELETE" />
+        <Button className="btns mr-3" label="SAVE" />
+      </div>
+    );
+  };
+  const renderHeader = (name) => {
+    return (
+      <div>
+        <Button
+          className="btns mr-3"
+          label="DEMO"
+          onClick={() => onClick("displayResponsive")}
+        />
+        <Button
+          className="btns mr-3"
+          label="IMPORT"
+          onClick={() => onClick("displayResponsive")}
+        />
+      </div>
+    );
+  };
+  const [data, setData] = useState(temp);
+  useEffect(() => {
+    let cnt = data.length - 1;
+    console.log(cnt);
+    if (cnt > 10) {
+      if (
+        data[cnt].type !== "" ||
+        data[cnt].kapan !== "" ||
+        data[cnt].refno !== "" ||
+        data[cnt].clarity !== "" ||
+        data[cnt].shape !== "" ||
+        data[cnt].size !== "" ||
+        data[cnt].lotno !== "" ||
+        data[cnt].certno !== "" ||
+        data[cnt].remark !== ""
+      ) {
+        let newData = [
+          ...data,
+          {
+            type: "",
+            kapan: "",
+            refno: "",
+            clarity: "",
+            shape: "",
+            size: "",
+            lotno: "",
+            certno: "",
+            remark: "",
+          },
+        ];
+        setData(newData);
+      }
+    }
+  }, [data]);
+  const setDataOnChange = (feild, index, value) => {
+    let mg = data;
+    console.log();
+    if (feild === "type") {
+      mg[index].type = value;
+    }
+    if (feild === "kapan") {
+      mg[index].kapan = value;
+    }
+    if (feild === "refno") {
+      mg[index].refno = value;
+    }
+    if (feild === "color") {
+      mg[index].color = value;
+    }
+    if (feild === "clarity") {
+      mg[index].clarity = value;
+    }
+    if (feild === "shape") {
+      mg[index].shape = value;
+    }
+    if (feild === "size") {
+      mg[index].size = value;
+    }
+    if (feild === "lotno") {
+      mg[index].lotno = value;
+    }
+    if (feild === "certno") {
+      mg[index].certno = value;
+    }
+    if (feild === "remark") {
+      mg[index].remark = value;
+    }
+    setData([...mg]);
+  };
+  const cities = [
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
+  ];
+  const onCityChange2 = (e) => {
+    setSelectedCity2(e.value);
+  };
+  return (
+    <div className="grid col-12 btnparent my-1">
+      <Button
+        className="btns mr-3"
+        label="ADD LOT"
+        onClick={() => onClick("displayResponsive")}
+      />
+      <Dialog
+        header={renderHeader("displayResponsive")}
+        visible={displayResponsive}
+        onHide={() => onHide("displayResponsive")}
+        breakpoints={{ "960px": "75vw" }}
+        style={{ width: "73%" }}
+        footer={renderFooter("displayResponsive")}
+      >
+        <div className="col-12 model-tbl pb-0 p-0 p-fluid">
+          <table className="data-table ">
+            <thead>
+              <tr>
+                <th>type</th>
+                <th>kapan</th>
+                <th>refno</th>
+                <th>color</th>
+                <th>clarity</th>
+                <th>shape</th>
+                <th>size</th>
+                <th>lotno</th>
+                <th>certno</th>
+                <th>remark</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((eliment, index) => {
+                return (
+                  <tr key={"lot" + index}>
+                    <td>
+                      <Dropdown
+                        value={selectedCity2}
+                        options={cities}
+                        onChange={onCityChange2}
+                        optionLabel="name"
+                        editable
+                      />
+                    </td>
+                    {/* <td>
+                      <input
+                        type="text"
+                        name="type"
+                        onChange={(e) => {
+                          setDataOnChange("type", index, e.target.value);
+                        }}
+                        value={eliment.type}
+                      ></input>
+                    </td> */}
+                    <td>
+                      <input
+                        type="text"
+                        name="kapan"
+                        onChange={(e) => {
+                          setDataOnChange("kapan", index, e.target.value);
+                        }}
+                        value={eliment.kapan}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="refno"
+                        onChange={(e) => {
+                          setDataOnChange("refno", index, e.target.value);
+                        }}
+                        value={eliment.refno}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="color"
+                        onChange={(e) => {
+                          setDataOnChange("color", index, e.target.value);
+                        }}
+                        value={eliment.color}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="clarity"
+                        onChange={(e) => {
+                          setDataOnChange("clarity", index, e.target.value);
+                        }}
+                        value={eliment.clarity}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="shape"
+                        onChange={(e) => {
+                          setDataOnChange("shape", index, e.target.value);
+                        }}
+                        value={eliment.shape}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="size"
+                        onChange={(e) => {
+                          setDataOnChange("size", index, e.target.value);
+                        }}
+                        value={eliment.size}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="lotno"
+                        onChange={(e) => {
+                          setDataOnChange("lotno", index, e.target.value);
+                        }}
+                        value={eliment.lotno}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="certno"
+                        onChange={(e) => {
+                          setDataOnChange("certno", index, e.target.value);
+                        }}
+                        value={eliment.certno}
+                      ></input>
+                    </td>
+                    <td>
+                      <input
+                        type="text"
+                        name="remark"
+                        onChange={(e) => {
+                          setDataOnChange("remark", index, e.target.value);
+                        }}
+                        value={eliment.remark}
+                      ></input>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-    )
-}
+      </Dialog>
+      <Button className="btns mr-3" label="DEMO" />
+      <Button className="btns mr-3" label="IMPORT" />
+    </div>
+  );
+};
